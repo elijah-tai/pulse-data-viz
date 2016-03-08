1aa1ccb7103e7c9d6f6051ab88b3d4091ae1e4b0
WonJune Tai <wonjune.tai@gmail.com>
2016-03-08 18:15:12 -0500

    Working on self-sorting table

--- pulse/pulseTable.js
+++ pulse/pulseTable.js
@@ -5,8 +5,10 @@ if (!d3.chart) d3.chart = {}
 d3.chart.table = function() {
 	var g
 	var data
-	var width = 300
-	var height = 500
+	var margin = {top: 20, right: 30, bottom: 30, left: 40},
+			width = 600 - margin.left - margin.right,
+			height = 500 - margin.top - margin.bottom
+
 	var dispatch = d3.dispatch(chart, "clicked")
 
 	function chart(container) {
@@ -17,34 +19,104 @@ d3.chart.table = function() {
 	chart.update = update
 
 	function update() {
-		var table = g.append("table"),
-				thead = table.append("thead"),
-				tbody = table.append("tbody")
+		var table = g.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
 
-		var columns = ["index", "transcript", "protein", "probability"]
+		var headerGrp = table.append("table").attr("class", "headerGrp")
+		var headerRow = headerGrp.append("thead")
+
+		var rowsGrp = table.append("table").attr("class", "rowsGrp")
+
+		var indexFieldWidth = 60,
+				transcriptFieldWidth = 140,
+				proteinFieldWidth = 105,
+				probabilityFieldWidth = 50
+
+		var	fieldHeight = 30
 		
-		// Create the column headers
-		thead.append("tr")
-			.selectAll("td")
-			.data(columns)
-			.enter()
-			.append("th")
-				.text(column => column)
+		transform("index")
 
-		// create rows for each object
-		var rows = tbody.selectAll("tr")
-			.data(data)
-			.enter()
-			.append("tr")
+		function transform(sortOn) {
+			table.selectAll("tr").remove()
 
-		var cells = rows.selectAll("td")
-			.data(row => columns.map(function(column) {
-				return {column: column, value: row[column]}
-			}))
-			.enter()
-			.append("td")
-			.attr("style", "font-family: Courier")
-			.html(d => d.value)
+			// Draw header
+			var header = headerRow.selectAll("thead")
+				.data(dataToArray(data[0]))
+				.enter().append("th")
+				.attr("class", (d, i) => "header" + i)
+				.on("click", function(d, i) {
+					transform(d[0])
+				})
+				.text(d => d[0])
+
+			// start filling the table
+			var rows = rowsGrp.selectAll("tr")
+				.data(data)
+				.enter().append("tr")
+				.attr("class", "row")
+
+			var rowsSort = rows.transition().duration(500)
+				.sort((a, b) => sort(a[sortOn], b[sortOn]))
+
+			var cells = rows.selectAll("td")
+				.data(d => dataToArray(d))
+				.enter().append("td")
+				.attr("class", (d, i) => "cell" + i)
+				.on("click", (d, i) => transform(d[0]))
+				.append("text")
+				.text(d => d[1])
+
+			// Let's reformat some of the columns by index value
+			// Might be best to move this CSS
+			// Column 0
+			headerGrp.selectAll(".header0")
+				.attr("width", indexFieldWidth)
+			rowsGrp.selectAll(".cell0")
+				.attr("width", indexFieldWidth)
+
+			// Column 1
+			headerGrp.selectAll(".header1")
+				.attr("width", transcriptFieldWidth)
+			rowsGrp.selectAll(".cell1")
+				.attr("width", transcriptFieldWidth)
+
+			// Column 2
+			headerGrp.selectAll(".header2")
+				.attr("width", proteinFieldWidth)
+			rowsGrp.selectAll(".cell2")
+				.attr("width", proteinFieldWidth)
+
+			// Column 3
+			headerGrp.selectAll(".header3")
+				.attr("width", probabilityFieldWidth)
+			rowsGrp.selectAll(".cell3")
+				.attr("width", probabilityFieldWidth)	
+		}			
+
+		function sort(a, b) {
+			if (typeof a == "string") {
+				return a.localeCompare(b)
+			}
+			else if (typeof a == "number") {
+				return a > b ? 1 : a == b ? 0 : -1
+			}
+		}
+
+		function dataKeyValueToArray(k, v) {
+			return [k, v]
+		}
+
+		function dataToArray(data) {
+			var result = new Array()
+			var key
+			for (key in data) {
+				if (data.hasOwnProperty(key)) {
+					result.push(dataKeyValueToArray(key, data[key]))
+				}
+			}
+			return result
+		}
+
 	}
 
 	chart.data = function(value) {
@@ -65,5 +137,7 @@ d3.chart.table = function() {
 		return chart
 	}
 
+	// returns dispatch method bound to chart (target) 
+	// with "on" name
 	return d3.rebind(chart, dispatch, "on")
 }
