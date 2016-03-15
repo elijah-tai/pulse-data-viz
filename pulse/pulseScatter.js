256f2ae36ee7ffcdab7ee810d93173819eac38d6
WonJune Tai <wonjune.tai@gmail.com>
2016-03-15 13:51:59 -0400

    Adds label, zoom, and tip features back

--- pulse/pulseScatter.js
+++ pulse/pulseScatter.js
@@ -5,20 +5,33 @@ if (!d3.chart) d3.chart = {}
 d3.chart.scatter = function() {
   var g
   var data
+  var rect
+  var xaxis
+  var yaxis
   var margin = { top: 50, right: 30, bottom: 50, left: 50}
   var width = scatterWidth - margin.left - margin.right,
       height = scatterHeight - margin.top - margin.bottom
+
+  var xCat = "index",
+      xLabel = "protein number",
+      yCat = "probability",
+      yLabel = "probability"
 
   var dispatch = d3.dispatch(chart, "clicked")
 
   function chart(container) {
     g = container
-    g.append("g")
-      .classed("xaxis", true)
+
+    // Build box for chart
+    rect = g.append("rect")
+      .attr("width", width)
+      .attr("height", height)
 
     g.append("g")
-      .classed("yaxis", true)
+      .classed("x axis", true)
 
+    g.append("g")
+      .classed("y axis", true)
     update()
   }
 
@@ -26,18 +39,11 @@ d3.chart.scatter = function() {
 
   function update() {
 
-    // Build x axis
-    // predefined category and label
-    var xCat = "index",
-        xLabel = "protein number"
-
-    // calculate range for x axis
+    // x axis
     var xMax = d3.max(data, d => d[xCat]) * 1.05,
         xMin = d3.min(data, d => d[xCat]),
-        // If xMin > 0, set xMin = 0, else xMin
         xMin = xMin > 0 ? 0 : xMin
 
-    // build scale for x axis
     var xScale = d3.scale.linear().range([0, width]).nice()
     xScale.domain([xMin, xMax])
 
@@ -45,31 +51,23 @@ d3.chart.scatter = function() {
                       .scale(xScale)
                       .orient("bottom")
 
-    // Attach x axis and label
-    var xg = g.select(".xaxis")
-        .classed("x axis", true)
-        .attr("transform", "translate(0," + scatterHeight + ")")
+    var xg = g.select(".x.axis")
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
       .append("text")
         .classed("label", true)
         .attr("text-anchor", "end")
-        .attr("x", scatterWidth)
+        .attr("x", width)
         .attr("y", margin.bottom - 10)
         .text(xLabel)
 
-    // Build y axis
-    // predefined category and label
-    var yCat = "probability",
-        yLabel = "probability"
 
-    // calculate range for y axis
+    // y axis
     var yMax = d3.max(data, d => d[yCat]) * 1.05,
-        // If yMax < 1, set yMax = 1, else, yMax
         yMax = yMax < 1 ? 1 : yMax,
         yMin = d3.min(data, d => d[yCat]),
         yMin = yMin > 0 ? 0 : yMin
 
-    // Build scale for y axis
     var yScale = d3.scale.linear().range([height, 0])
     yScale.domain([yMin, yMax])
 
@@ -77,9 +75,7 @@ d3.chart.scatter = function() {
                       .scale(yScale)
                       .orient("left")
 
-    // Attach y axis and label
-    var yg = d3.select(".yaxis")
-       .classed("y axis", true)
+    var yg = g.select(".y.axis")
        .call(yAxis)
       .append("text")
        .classed("label", true)
@@ -88,16 +84,6 @@ d3.chart.scatter = function() {
        .attr("dy", ".75em")
        .attr("transform", "rotate(-90)")
        .text(yCat)
-
-    // Initialize tooltip
-    var tip = d3.tip()
-                .attr("class", "d3-tip")
-                .offset([-10, 0])
-                .html(
-                  d => "transcript: " + d["transcript"] + "<br>" 
-                  + "protein: " + d["protein"] + "<br>" 
-                  + yCat + ": " + d[yCat]
-                )
 
     // Initialize zoom behaviour
     var zoomBehavior = d3.behavior.zoom()
@@ -113,9 +99,36 @@ d3.chart.scatter = function() {
       .attr("width", width)
       .attr("height", height)
 
+    g.append("svg:line")
+      .classed("axisLine hAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", width)
+      .attr("y2", 0)
+      .attr("transform", "translate(0," + scatterWidth + ")")
+
+    objects.append("svg:line")
+      .classed("axisLine vAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 0)
+      .attr("y2", scatterHeight)
+
     var clickData = {
       isActive: false
     }
+
+    // Initialize tooltip
+    var tip = d3.tip()
+                .attr("class", "d3-tip")
+                .offset([-10, 0])
+                .html(
+                  d => "transcript: " + d["transcript"] + "<br>" 
+                  + "protein: " + d["protein"] + "<br>" 
+                  + yCat + ": " + d[yCat]
+                )
+
+    g.call(tip)
 
     var drawnData = objects.selectAll(".dot")
         .data(data)
@@ -171,10 +184,10 @@ d3.chart.scatter = function() {
     }
 
     function zoom() {
-      svg.select(".x.axis").call(xAxis)
-      svg.select(".y.axis").call(yAxis)
+      g.select(".x.axis").call(xAxis)
+      g.select(".y.axis").call(yAxis)
 
-      svg.selectAll(".dot")
+      g.selectAll(".dot")
           .attr("transform", transform)
     }
 
