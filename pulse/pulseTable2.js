4e489ce906ac91a4982d1cc1a677f4fc27a136fb
WonJune Tai <wonjune.tai@gmail.com>
2016-04-11 00:04:37 -0400

    Addresses issue #14

--- pulse/pulseTable2.js
+++ pulse/pulseTable2.js
@@ -7,13 +7,18 @@ d3.chart.table2 = function() {
 	var g
 	var svg
 	var data
-	var margin = {top: 20, right: 30, bottom: 30, left: 40},
+	var margin = {top: 20, right: 20, bottom: 20, left: 20},
 			tableWidth = 1000 - margin.left - margin.right,
-			tableHeight = 400 - margin.top - margin.bottom,
+			tableHeight,
 			elmHeight = 10,
 			pfamHeight = 10
 
-	var widthScale,
+	var tip
+	var elmTip
+	var pfamTip
+
+	var xScale,
+			widthScale,
 			yScale
 
 	var clickData = {
@@ -25,6 +30,33 @@ d3.chart.table2 = function() {
 
 	function chart(container) {
 		g = container
+
+		// g.append("g")
+		// 	.classed("y table-axis", true)
+
+		// Initialize tooltips
+		elmTip = d3.tip()
+			.attr("class", "d3-tip-table")
+			.offset([-10, 0])
+			.html(
+				function(d) {
+					return "elm_id: " + d.elm.elm_id + "<br>" 
+					+ "start: " + d.elm.start + "<br>" 
+					+ "end: " + d.elm.end
+				}
+			)
+
+		pfamTip = d3.tip()
+			.attr("class", "d3-tip-table")
+			.offset([-10, 0])
+			.html(
+				function(d) {
+					return "pfam_id: " + d.pfam.pfam_id + "<br>"
+					+ "start: " + d.pfam.start + "<br>" 
+					+ "end: " + d.pfam.end + "<br>"
+					+ "Click to go to website"
+				}
+			)
 		update()
 	}
 
@@ -45,38 +77,71 @@ d3.chart.table2 = function() {
 		chart.refreshTable = refreshTable
 		svg = g.append("svg")
 			.attr("width", tableWidth)
-			.attr("height", tableHeight)
 
 		// TODO: Need to implement d3.scale for transcript widths
 		function refreshTable(d) {
 			if (svg) {
 				svg.selectAll("rect").remove()
+				svg.selectAll("text").remove()
 			}
 			if (d != null) {
 				data = data.filter(p => d["protein"] === p["protein"])
 
-				// dynamic tableHeight
-				tableHeight = data.length * 30
+				// dynamic tableHeight and scales
+				tableHeight = data.length * 60
+				svg.attr("height", tableHeight)
 
-				var maxWidth = d3.max(data, d => d.width)
-				
-				widthScale = d3.scale.linear()
-									.domain([0, maxWidth])
+				svg.call(elmTip)
+				svg.call(pfamTip)
+
+				var widthMax = d3.max(data, d => d.width)
+				var xMin = d3.min(data, d => d.start)
+				var xMax = d3.max(data, d => d.end)
+
+				xScale = d3.scale.linear()
+									.domain([xMin, xMax])
 									.range([0, tableWidth])
-
+									.nice()
+				widthScale = d3.scale.linear()
+									.domain([0, widthMax])
+									.range([0, tableWidth])
 				yScale = d3.scale.linear()
 									.domain([0, data.length])
-									.range([0, tableHeight])
+									.range([margin.top, tableHeight])
 
+				// xAxis
 				var xAxis = d3.svg.axis()
-											.scale(widthScale)
-											.orient("bottom")
-				
-				var yAxis = d3.svg.axis()
-											.scale(yScale)
-											.orient("left")
+										.scale(xScale)
+										.orient("bottom")
+										.ticks(10)
+
+				svg.selectAll("g.x-table-axis").remove()
+				svg.selectAll("line").remove()
+				svg.append("g")
+					.classed("x-table-axis", true)
+
+				var xg = g.select(".x-table-axis")
+						.attr("transform", "translate(0," + (tableHeight - margin.bottom) + ")")
+						.call(xAxis)
+
+				svg.append("line")
+					.classed("axisLine hAxisLine", true)
+					.attr("x1", 0)
+					.attr("y1", tableHeight - margin.bottom)
+					.attr("x2", tableWidth)
+					.attr("y2", tableHeight - margin.bottom)
+					.attr("transform", "translate(0," + tableWidth + ")")
+
+				// // yAxis
+				// var yAxis = d3.svg.axis()
+				// 					.scale(yScale)
+				// 					.orient("left")
+
+				// var yg = g.select(".y.table-axis")
+				// 	 					.call(yAxis)
 
 				// console.log(data)
+
 
 				// draw basic rectangle for protein
 				var transcripts = svg.selectAll("rect")
@@ -93,48 +158,66 @@ d3.chart.table2 = function() {
 					.attr("fill", "#FE9949")
 					.attr("height", "10")
 
-				// overlay pfam data in red
-				// data.forEach(function(d, index) {
-				// 	d.elms.forEach(function(elm) {
-				// 		svg.datum(function() {
-				// 			// console.log(index, elm)
-				// 			return {index: index, elm: elm}
-				// 		})
-				// 			.append("rect").classed("elm-label", true)
-				// 			.attr("x", function(d) {
-				// 				return d.elm.start
-				// 			})
-				// 			.attr("y", function(d, index) {
-				// 				return (d.index * (tableHeight / data.length))
-				// 			})
-				// 			.attr("width", function(d) {
-				// 				return d.elm.end - d.elm.start
-				// 			})
-				// 			.attr("rx", 6)
-				// 			.attr("ry", 6)
-				// 			.attr("height", "10")
-				// 			.attr("fill", "#1B9AF7")
-				// 	})
-					
-				// 	// d.pfam.forEach(function(pfam) {
-				// 	// 	svg.datum(pfam)
-				// 	// 		.append("rect").classed("pfam-label", true)
-				// 	// 		.attr("x", function(pfam) {
-				// 	// 			return pfam.start
-				// 	// 		})
-				// 	// 		.attr("y", function(pfam, index) {
-				// 	// 			return (index * 40 + 20)
-				// 	// 		})
-				// 	// 		.attr("width", function(pfam) {
-				// 	// 			return pfam.end - pfam.start
-				// 	// 		})
-				// 	// 		.attr("rx", 6)
-				// 	// 		.attr("ry", 6)
-				// 	// 		.attr("height", "10")
-				// 	// 		.attr("fill", "#FC880F")
-				// 	// })
-				// })
+				svg.selectAll("text").remove()
+				var labels = svg.selectAll("text")
+					.data(data)
+					.enter()
+					.append("text")
+						.attr("x", 0)
+						.attr("y", (d, i) => yScale(i))
+						.text(d => d.entry)
 
+				// overlay pfam and elm data in red
+				data.forEach(function(d, index) {
+					// console.log(d)
+					d.elms.forEach(function(elm) {
+						svg.datum(function() {
+							// console.log(index, elm)
+							return {index: index, elm: elm}
+						})
+							.append("rect").classed("elm-label", true)
+							.attr("x", function(d) {
+								return d.elm.start
+							})
+							.attr("y", function(d, index) {
+								return yScale(d.index) + 10
+							})
+							.attr("width", function(d) {
+								return d.elm.end - d.elm.start
+							})
+							.attr("rx", 6)
+							.attr("ry", 6)
+							.on("mouseover", elmTip.show)
+							.on("mouseout", elmTip.hide)
+							.attr("height", "10")
+							.attr("fill", "#1B9AF7")
+					})
+					
+					d.pfam.forEach(function(pfam) {
+						svg.datum(function() {
+							return {index: index, pfam: pfam}
+						})
+							.append("rect").classed("pfam-label", true)
+							.attr("x", function(d) {
+								return d.pfam.start
+							})
+							.attr("y", function(d, index) {
+								return yScale(d.index) + 20
+							})
+							.attr("width", function(d) {
+								return d.pfam.end - d.pfam.start
+							})
+							.attr("rx", 6)
+							.attr("ry", 6)
+							.on("mouseover", pfamTip.show)
+							.on("mousedown", function(d) {
+								window.open('http://pfam.xfam.org/family/' +  d.pfam.pfam_id, '_blank')
+							})
+							.on("mouseout", pfamTip.hide)
+							.attr("height", "10")
+							.attr("fill", "#49E845")
+					})
+				})
 			}
 		}
 	}
